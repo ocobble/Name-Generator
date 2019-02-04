6147bc0896f55381b6bfaa16eefc4a090cd39baa
Olivia Cobble <ocobble@hotmail.com>
2019-02-04 15:18:53 -0600

    Changed JScroller to JComboBox

--- src/Naruto_Frame.java
+++ src/Naruto_Frame.java
@@ -1,10 +1,15 @@
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -14,7 +19,9 @@ import javax.swing.JList;
 
 public class Naruto_Frame extends JFrame
 {
-    JPanel questionsPanel = new JPanel(new GridLayout(4,1,5,20));
+    //JPanel questionsPanel = new JPanel(new GridLayout(4,1,5,20));
+
+	JPanel questionsPanel;
     JPanel ninjafyPanel = new JPanel();
     JPanel namePanel = new JPanel();
     JPanel villagePanel = new JPanel();
@@ -28,12 +35,10 @@ public class Naruto_Frame extends JFrame
     JLabel lastLabel = new JLabel("Enter last name");
     JLabel hogwartsLabel = new JLabel("Select your hogwarts house");
     JLabel lessAwkward = new JLabel("You are:" );
+    JLabel errorLabel = new JLabel();
     
     String[] houses = {"Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin", "idk man"};
-    
-    
-    JList<String> hogwartsHouses = new JList<String>(houses);
-    JScrollPane scroller = new JScrollPane(hogwartsHouses);
+    JComboBox<String> houseScroller = new JComboBox<String>(houses);
     
     JButton ninjafy = new JButton("Ninjafy!");
     
@@ -48,6 +53,7 @@ public class Naruto_Frame extends JFrame
         super(title);
         setLayout(new GridLayout(3, 0));
         
+        
         firstLabel.setHorizontalAlignment(JLabel.LEFT);
         lastLabel.setHorizontalAlignment(JLabel.LEFT);
         hogwartsLabel.setHorizontalAlignment(JLabel.LEFT);
@@ -58,6 +64,97 @@ public class Naruto_Frame extends JFrame
         lessAwkward.setHorizontalAlignment(JLabel.CENTER);
         ninjafy.setHorizontalAlignment(JButton.CENTER);
         
+        
+        GridBagLayout gridbag = new GridBagLayout();
+        GridBagConstraints c = new GridBagConstraints();
+
+        questionsPanel = new JPanel(gridbag);
+        //setFont(new Font("SansSerif", Font.PLAIN, 14));
+        //setLayout(gridbag);
+
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 1;
+        c.gridwidth = 1;                //reset to the default
+        c.gridheight = 1;
+        c.ipady = 0;
+        
+        
+        gridbag.setConstraints(firstLabel, c);
+        questionsPanel.add(firstLabel);
+        
+        c.gridwidth = GridBagConstraints.REMAINDER;
+        c.weightx = 1;
+        //c.ipady = 2;
+        
+        gridbag.setConstraints(firstName, c);
+        questionsPanel.add(firstName);
+        
+        c.gridwidth = 1;
+        //c.ipady = 1;
+        
+        gridbag.setConstraints(lastLabel, c);
+        questionsPanel.add(lastLabel);
+        
+        c.gridwidth = GridBagConstraints.REMAINDER;
+        //c.ipady = 2;
+        
+        gridbag.setConstraints(lastName, c);
+        questionsPanel.add(lastName);
+        
+        c.gridwidth = 1;
+        //c.ipady = 1;
+        
+        gridbag.setConstraints(hogwartsLabel, c);
+        questionsPanel.add(hogwartsLabel);
+        
+        c.gridwidth = GridBagConstraints.REMAINDER;
+        //c.ipady = 2;
+        
+        gridbag.setConstraints(houseScroller, c);
+        questionsPanel.add(houseScroller);
+        
+        //c.weightx = 500;
+        c.ipadx = 50;
+        c.fill = GridBagConstraints.NONE;
+        c.gridheight = 3;
+        
+        gridbag.setConstraints(ninjafy, c);
+        questionsPanel.add(ninjafy);
+        
+        //gridbag.setConstraints(errorLabel, c);
+        //questionsPanel.add(errorLabel);
+        //gridbag.setConstraints(firstName, c);
+        //add(firstName);
+        
+        
+        /*
+        makebutton("Button3", gridbag, c);
+
+        c.gridwidth = GridBagConstraints.REMAINDER; //end row
+        makebutton("Button4", gridbag, c);
+
+        c.weightx = 0.0;                //reset to the default
+        makebutton("Button5", gridbag, c); //another row
+
+        c.gridwidth = GridBagConstraints.RELATIVE; //next-to-last in row
+        makebutton("Button6", gridbag, c);
+
+        c.gridwidth = GridBagConstraints.REMAINDER; //end row
+        makebutton("Button7", gridbag, c);
+
+        c.gridwidth = 1;                //reset to the default
+        c.gridheight = 2;
+        c.weighty = 1.0;
+        makebutton("Button8", gridbag, c);
+
+        c.weighty = 0.0;                //reset to the default
+        c.gridwidth = GridBagConstraints.REMAINDER; //end row
+        c.gridheight = 1;               //reset to the default
+        makebutton("Button9", gridbag, c);
+        makebutton("Button10", gridbag, c);
+
+        setSize(300, 100);
+        */
         JLabel leafLabel = new JLabel(leaf);
         //villagePanel.add(leafLabel);
         JLabel cloudLabel = new JLabel(cloud);
@@ -67,6 +164,7 @@ public class Naruto_Frame extends JFrame
         
         
         
+        /*
         questionsPanel.add(firstLabel);
         questionsPanel.add(firstName);
         questionsPanel.add(lastLabel);
@@ -76,9 +174,12 @@ public class Naruto_Frame extends JFrame
         questionsPanel.add(scroller);
         
         questionsPanel.add(ninjafy);
+        */
         
         namePanel.add(lessAwkward);
         namePanel.add(ninjaName);
+        
+        villagePanel.add(errorLabel);
         
         this.add(questionsPanel);
         //this.add(ninjafyPanel);
@@ -95,10 +196,19 @@ public class Naruto_Frame extends JFrame
                 
                 ninjaFirst = Name_Helper.ninjafy(firstName.getText());
                 ninjaLast = Name_Helper.ninjafy(lastName.getText());
-                ninjaVillage = Name_Helper.villageIdentifier(hogwartsHouses.getSelectedValue());
+                ninjaVillage = Name_Helper.villageIdentifier((String) houseScroller.getSelectedItem());
                 
-                ninjaName.setText(ninjaFirst + " " + ninjaLast + " of the " + ninjaVillage);
+                if (!ninjaVillage.equals("None"))
+                {
+                	ninjaName.setText(ninjaFirst + " " + ninjaLast + " of the " + ninjaVillage);
+                }
+                
+                else {
+                	errorLabel.setText("Please select a Hogwarts house");
+                }
+                
                 villagePanel.removeAll();
+                
                 switch(ninjaVillage)
                 {
                     case("Leaf"):
@@ -122,13 +232,23 @@ public class Naruto_Frame extends JFrame
                         fave = Color.ORANGE;
                         break;
                 }
-                questionsPanel.setBackground(fave);
-                villagePanel.setBackground(fave);
-                ninjafyPanel.setBackground(fave);
-                namePanel.setBackground(fave);
-                questionsPanel.setForeground(Color.BLACK);
-                villagePanel.updateUI();
                 
+                if (!ninjaVillage.equals("None"))
+                {
+                	questionsPanel.setBackground(fave);
+                	villagePanel.setBackground(fave);
+                	ninjafyPanel.setBackground(fave);
+                	namePanel.setBackground(fave);
+                	questionsPanel.setForeground(Color.BLACK);
+                	
+                }
+                else
+                {
+                	villagePanel.add(errorLabel);
+                	errorLabel.setText("Please select a Hogwarts house");
+                }
+                
+                villagePanel.updateUI();
             }
             
                 });
